a0f2bbb4939a4121b49322c7b9f14311ee3d632b
sastiam <sfranciach@gmail.com>
2021-08-23 15:06:34 -0500

    fix numbers repeat in BingoCard

--- BingoCard.js
+++ BingoCard.js
@@ -3,34 +3,40 @@ const _ = require("underscore");
 /*
 It receives as a parameter a minimum and maximum number, returning as a result an array of 5 elements with numbers within that range of the two numbers */
 const generateArrayNumbers = (min, max) => {
-  return Array.from(Array(5), () => _.random(min, max));
+  let passedArray = false;
+  let newArr;
+  while (!passedArray) {
+    newArr = Array.from(Array(5), () => _.random(min, max));
+    let notDuplicates = new Set(newArr).size === newArr.length;
+    if (notDuplicates) {
+      passedArray = true;
+    }
+  }
+  return newArr;
 };
 
-const BingoCard = function() {
-    this.card = {};
-}
+const BingoCard = function () {
+  this.card = {};
+};
 
-BingoCard.prototype.generateNewCard = function() {
-    // An array is created with the bingo rows and a minimum and maximum value is assigned by default
-    let columns = ['B', 'I', 'N', 'G', 'O'];
-    let minDefault = 1;
-    let maxDefault = 15;
+BingoCard.prototype.generateNewCard = function () {
+  // An array is created with the bingo rows and a minimum and maximum value is assigned by default
+  let columns = ["B", "I", "N", "G", "O"];
+  let minDefault = 1;
+  let maxDefault = 15;
 
-    // It goes through the columns where the random numbers are generated in the card object
-    columns.forEach(row => {
-        this.card[row] = generateArrayNumbers(minDefault, maxDefault);
-        minDefault = maxDefault + 1;
-        maxDefault = maxDefault + 15;
-    })
+  // It goes through the columns where the random numbers are generated in the card object
+  columns.forEach((row) => {
+    this.card[row] = generateArrayNumbers(minDefault, maxDefault);
+    minDefault = maxDefault + 1;
+    maxDefault = maxDefault + 15;
+  });
 
-    // The intermediate value of the intermediate column is changed by the word 'FREE'
-    let middleColumn = this.card['N'];
-    middleColumn = [...middleColumn, middleColumn[2] = 'FREE']
+  // The intermediate value of the intermediate column is changed by the word 'FREE'
+  let middleColumn = this.card["N"];
+  middleColumn = [...middleColumn, (middleColumn[2] = "FREE")];
 
-    return this.card;
-}
+  return this.card;
+};
 
-
-
-
-module.exports = BingoCard
+module.exports = BingoCard;
--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-const Player = require('./Player');
-const Game = require('./Game');
-
+const Player = require("./Player");
+const Game = require("./Game");
 
 const bingoGame = new Game();
-let initBingoGame = bingoGame.init()
-let players = new Array(new Player('Sebastian'));
+let initBingoGame = bingoGame.init();
+let players = new Array(
+  new Player("Sebastian"),
+  new Player("Jugador 2"),
+  new Player("Jugador 3"),
+  new Player("Jugador 4")
+);
 
 let numberCalled = initBingoGame.next().value;
 let winnerFound = false;
 
-while(numberCalled && !winnerFound) {
-    console.log(numberCalled);
-    for(let player in players) {
-        players[player].checkNumberCalled(numberCalled);
-        let checkWinner = bingoGame.checkPlayerBingo(players[player]);
-        if(checkWinner) {
-            winnerFound = true;
-            return `Player ${players[player].name} has won the game`
-        }
-        console.log('Not winner found');
+// force winner
+let winner = players[0];
+winner.useFreeNumber();
+
+while (numberCalled && !winnerFound) {
+  console.log(numberCalled);
+  for (let player in players) {
+    players[player].checkNumberCalled(numberCalled);
+    let checkWinner = bingoGame.checkPlayerBingo(players[player]);
+    if (checkWinner) {
+      winnerFound = true;
+      return `Player ${players[player].name} has won the game`;
     }
-    numberCalled = initBingoGame.next().value;
+  }
+  numberCalled = initBingoGame.next().value;
 }
+console.log("Not winner found");
